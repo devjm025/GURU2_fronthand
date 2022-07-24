e9582c8004d5562ceb50cb64d91b9793bf50bb11
devjm025 <wjm00627@swu.ac.kr>
2022-07-24 16:33:33 +0900

    2022.07.24

---
+++ app/src/main/java/com/android/appdev/JoinActivity.kt
@@ -0,0 +1,11 @@
+package com.android.appdev
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class JoinActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_join)
+    }
+}
