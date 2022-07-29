d5245c134b0b458861536896a6b11684e6bc4641
devjm025 <wjm00627@swu.ac.kr>
2022-07-29 11:08:34 +0900

    2022.07.29 우지민 AddList.kt 구현

---
+++ app/src/main/java/com/android/appdev/DBManager.kt
@@ -0,0 +1,21 @@
+package com.android.appdev
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class DBManager (
+    context: Context?,
+    name: String?,
+    factory: SQLiteDatabase.CursorFactory?,
+    version: Int
+    ) : SQLiteOpenHelper(context, name, factory, version) {
+        override fun onCreate(db: SQLiteDatabase?) {
+
+            db!!.execSQL("CREATE TABLE bucketlist(image text, category text, process INTEGER, Dday text, title text, info text)")
+        }
+
+        override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+
+        }
+}
