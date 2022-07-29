b352c70237513931883ee558cada5a88fede6391
suaJeong <studyingspacewhale@gmail.com>
2022-07-30 03:18:03 +0900

    2022.07.30 정수아 title 페이지 자동 전환 성공

--- app/src/main/java/com/android/appdev/MainActivity.kt
+++ app/src/main/java/com/android/appdev/MainActivity.kt
@@ -6,40 +6,22 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.widget.Button
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var Title_btn_Startbtn:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-        Title_btn_Startbtn=findViewById<Button>(R.id.Title_btn_Startbtn)
-
-        Title_btn_Startbtn.setOnClickListener {
-//            if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
-//                if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)){
-//                    var dlg = AlertDialog.Builder(this)
-//                    dlg.setTitle("권한이 필요한 이유")
-//                    dlg.setMessage("사진 정보를 얻기 위해서는 외부 저장소 권한이 필수로 필요합니다.")
-//                    dlg.setPositiveButton("확인"){dialog,which->
-//                        ActivityCompat.requestPermissions(this@MainActivity,
-//                        arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
-//                        1000)}
-//                    dlg.setNegativeButton("취소",null)
-//                    dlg.show()
-//                }else{
-//                    ActivityCompat.requestPermissions(this,
-//                        arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),1000)
-//                }
-//            }
-
-            var intent = Intent(this, AccountActivity::class.java)
+        var handler = Handler()
+        handler.postDelayed( {
+            var intent = Intent( this, AccountActivity::class.java)
             startActivity(intent)
-        }
+        }, 3000)
     }
+
 }
