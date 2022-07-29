e3990946c409b7a0664ffea45d30f7e287cb26de
suaJeong <studyingspacewhale@gmail.com>
2022-07-29 21:53:58 +0900

    2022_07_29 정수아 account, sign up, login 페이지 전환 추가

--- app/src/main/java/com/android/appdev/LoginActivity.kt
+++ app/src/main/java/com/android/appdev/LoginActivity.kt
@@ -1,11 +1,23 @@
 package com.android.appdev
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
+
+    lateinit var login_btn_LogIn : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        login_btn_LogIn=findViewById<Button>(R.id.login_btn_LogIn)
+
+        login_btn_LogIn.setOnClickListener{
+            var intent = Intent(this,ListActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/android/appdev/SignUpActivity.kt
+++ app/src/main/java/com/android/appdev/SignUpActivity.kt
@@ -1,11 +1,27 @@
 package com.android.appdev
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_signup.*
 
 class SignUpActivity : AppCompatActivity() {
+
+    lateinit var login_btn_SignUp : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
+        login_btn_SignUp=findViewById<Button>(R.id.login_btn_SignUp)
+
+        login_btn_SignUp.setOnClickListener{
+            var intent = Intent(this,ListActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
