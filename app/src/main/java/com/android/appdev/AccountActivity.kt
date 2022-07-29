7676dc39e17523e9c2f82cdaa7b68475628f9649
suaJeong <studyingspacewhale@gmail.com>
2022-07-29 23:56:51 +0900

    2022_07_29 정수아 Image 권한 관련 코드 activity_account.xml, .kt부분 AddList.kt 부분

--- app/src/main/java/com/android/appdev/AccountActivity.kt
+++ app/src/main/java/com/android/appdev/AccountActivity.kt
@@ -1,17 +1,28 @@
 package com.android.appdev
 
+import android.Manifest.permission_group.STORAGE
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import android.widget.Button
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.util.jar.Manifest
 
 class AccountActivity : AppCompatActivity() {
+
     lateinit var account_btn_LogIn: Button
     lateinit var account_btn_SignUp:Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_account)
+
+
 
 
         account_btn_LogIn=findViewById<Button>(R.id.account_btn_LogIn)
@@ -27,5 +38,23 @@ class AccountActivity : AppCompatActivity() {
             var intent = Intent(this,SignUpActivity::class.java)
             startActivity(intent)
         }
+
+        //getAllPhotos()
     }
+
+//    private fun getAllPhotos(){
+//        val cursor = contentResolver.query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+//            null,
+//            null,
+//            null,
+//        MediaStore.Images.ImageColumns.DATE_TAKEN+"DESC")
+//
+//        if(cursor != null){
+//            while(cursor.moveToNext()){
+//                val uri = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA))
+//                Log.d("AccountActivity",uri)
+//            }
+//            cursor.close()
+//        }
+//    }
 }
--- app/src/main/java/com/android/appdev/MainActivity.kt
+++ app/src/main/java/com/android/appdev/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.android.appdev
 
+import android.Manifest
+import android.app.AlertDialog
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,10 +17,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         Title_btn_Startbtn=findViewById<Button>(R.id.Title_btn_Startbtn)
 
-        Title_btn_Startbtn.setOnClickListener{
-            var intent = Intent(this,AccountActivity::class.java)
+        Title_btn_Startbtn.setOnClickListener {
+//            if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
+//                if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)){
+//                    var dlg = AlertDialog.Builder(this)
+//                    dlg.setTitle("권한이 필요한 이유")
+//                    dlg.setMessage("사진 정보를 얻기 위해서는 외부 저장소 권한이 필수로 필요합니다.")
+//                    dlg.setPositiveButton("확인"){dialog,which->
+//                        ActivityCompat.requestPermissions(this@MainActivity,
+//                        arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+//                        1000)}
+//                    dlg.setNegativeButton("취소",null)
+//                    dlg.show()
+//                }else{
+//                    ActivityCompat.requestPermissions(this,
+//                        arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),1000)
+//                }
+//            }
+
+            var intent = Intent(this, AccountActivity::class.java)
             startActivity(intent)
         }
     }
