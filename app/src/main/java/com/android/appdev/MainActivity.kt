d4a89475d420dc0a933bc4a71febe96d3ecc6609
suaJeong <studyingspacewhale@gmail.com>
2022-07-31 18:07:41 +0900

    2022.07.31 정수아 Title 페이지 완료

--- app/src/main/java/com/android/appdev/MainActivity.kt
+++ app/src/main/java/com/android/appdev/MainActivity.kt
@@ -8,20 +8,29 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.widget.Button
+import android.widget.ImageView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.fragment_photo.*
 
 class MainActivity : AppCompatActivity() {
+
+    //lateinit var gif_image: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //gif_image=findViewById(R.id.gif_image)
+
+        //Glide.with(this).load(R.raw.title_star).override(560,560).into()
+
         var handler = Handler()
         handler.postDelayed( {
             var intent = Intent( this, AccountActivity::class.java)
             startActivity(intent)
-        }, 3000)
+        }, 5000)
     }
 
 }
